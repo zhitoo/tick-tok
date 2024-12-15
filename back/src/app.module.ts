90f33d5d78b740af475f997994ecee6a0554deea
Hossein Shafee <zhitoooo@gmail.com>
2024-12-15 23:03:23 +0330

    create tables and format validation errors

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import {join} from 'path';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile:join(process.cwd(),'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      context:({req, res})=>({req, res}),
+    }),
+    ConfigModule.forRoot({})
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    allowedHeaders:['*'],
+    methods:['*']
+  });
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory:(errors)=>{
+        const formattedErrors = errors.reduce((acc, err)=>{
+          acc[err.property] = Object.values(err.constraints).join(
+            ', ',
+          );
+          return acc;
+        }, {});
+        throw new BadRequestException(formattedErrors);
+      }
+    })
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
